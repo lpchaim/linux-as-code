a465957d4237175f4c3de67c53cdd66285604697
Lucas Chaim <lpchaim@gmail.com>
2023-10-12 16:43:25 -0300

    feat: minimal deploy playbook
    Created a playbook which sets up a base development environment, with configurable defaults.
    It makes use of nix + home-manager and is intended as a minimal starting point with QoL tweaks and essential tools.

--- shell.nix
+++ shell.nix
@@ -2,9 +2,14 @@ with import <nixpkgs> {};
 
 mkShell {
   buildInputs = [
-    python311Packages.ansible-core
+    (python311.withPackages(ps: with ps; [
+      ansible-core
+    ]))
+    nixd
+    nixpkgs-fmt
   ];
   shellHook = ''
     export LC_ALL="C.UTF-8";
+    ansible-galaxy collection install -r requirements.yml > /dev/null;
   '';
 }
